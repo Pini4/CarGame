91bf868a6b7d3164494f5a118afcf66422319a38
Pini4 <User@WIN-GA1Q70LI9LP>
2022-11-01 19:39:08 +0300

    Добавьте файлы проекта.

---
+++ CarGame/Car.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarGame
+{
+    internal class Car
+    {
+        public string Model;
+        public int YearBuild;
+        public const float MaxSpeed = 320;
+        public int Run;
+        public int carkeys;
+        public int fiction;
+        public bool StartEngine;
+        public bool CarKey;
+
+        public Vector3 Position;
+
+        public Car()
+        {
+            Position = new Vector3();
+            Model = "Неизвестно";
+            YearBuild = 0000;
+            Run = 0;
+            StartEngine = false;
+            carkeys = 0110;
+            fiction = 0000;
+            CarKey = false;
+        }
+        public Car(string model, Vector3 pos)
+        {
+            Position = pos;
+            Model = model;
+            YearBuild = DateTime.Today.Year;
+            Run = 0;
+            StartEngine = false;
+        }
+       public void KeyIn()
+        {
+            Console.WriteLine("напишите 4x значный код ключа");
+            fiction = Convert.ToInt32(Console.ReadLine());
+            if (carkeys == fiction)
+            {
+                Console.WriteLine("Ключ вставлен");
+                CarKey = true;
+            }
+            else
+            {
+                Console.WriteLine("Ключ не подходит");
+            }
+
+        }
+        public void KeyOut()
+        {
+            CarKey = false;
+            Console.WriteLine("Ключ был вынут");
+        }
+        public void Start()
+        {
+            if (!CarKey)
+                return;
+            Console.WriteLine($"Двигатель машины {Model} включился");
+            StartEngine = true;
+        }
+
+        public void Off()
+        {
+            if (!CarKey)
+                return;
+            Console.WriteLine($"Двигатель машины  {Model} выключился");
+            StartEngine = false;
+        }
+
+        public void Move(Vector3 vector)
+        {
+            if (!StartEngine)
+                return;
+
+            Position.X += vector.X;
+            Position.Y += vector.Y;
+            Position.Z += vector.Z;
+            Run++;
+
+            Console.WriteLine($"Машина {Model} передвинулась на X: {Position.X}, Y: {Position.Y}, Z: {Position.Z}");
+        }
+
+        public string GetInfo() => $"Тачка {Model}. {YearBuild} - год производства. Двигатель заведен. Позиция = X: {Position.X}, Y: {Position.Y}, Z: {Position.Z}. Пробег = {Run}";
+    }
+}
+    
+
---
+++ CarGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using CarGame;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Vector3 vector = new Vector3(1, 2, 3);
+        Car audi = new Car("Audi", vector);
+        audi.KeyIn();
+        audi.Start();
+
+        audi.Move(new Vector3(0, 6, 5));
+        Console.WriteLine(audi.GetInfo());
+        audi.Move(new Vector3(0, 6, 5));
+        Console.WriteLine(audi.GetInfo());
+        audi.Move(new Vector3(0, 6, 5));
+        Console.WriteLine(audi.GetInfo());
+        audi.KeyOut();
+        audi.Off();
+
+
+
+
+    }
+}
